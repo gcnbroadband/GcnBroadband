089323a9476d7c7a1a9712b6b5557d0728ae33a8
gcnbroadband <gcnbroadbandindia@gmail.com>
2016-10-07 13:39:32 +0530

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,59 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require bootstrap-material-design
+//= require jquery.dropdown.js
+//= jquery.cycle.all.js
+//= require turbolinks
+//= welcome.js
+//= require_tree .
+
+
+
+function dropdown(){
+	$.material.init();
+
+
+	// Javascript for select contact box
+	
+
+	$('#career-options li').each(function(){
+		$(this).click(function(){
+			$('.ll').hide();
+			$('#'+$(this).val()).fadeIn();
+		});
+	});
+}
+ready = function() {
+	var ready;
+	var winHeight = $(window).height();
+	var footerHeight = $('.footer-gcn').height();
+	var finalHeight = winHeight-footerHeight;
+	console.log(finalHeight);
+	$(".wrapper-two").css('min-height', finalHeight);
+	$(".wrapper").height(winHeight);
+	var navHeight = $(".main-header").height();
+	var sliderHeight = winHeight-navHeight;
+	$(".pics").height(sliderHeight);
+	$("#plans-slider").height(sliderHeight);
+}
+
+$(document).on('turbolinks:load', ready);
+$(document).ready(ready);
+$(document).bind('page:change', ready);
+
+$(document).on('turbolinks:load', dropdown);
+$(document).ready(dropdown);
+$(document).bind('page:change', dropdown);
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,21 @@
+class WelcomeController < ApplicationController
+  def index
+  	
+  end
+
+  def about
+  	
+  end
+
+  def plan
+  	
+  end
+
+  def contact
+  	
+  end
+
+  def career
+  		
+  end	
+end
